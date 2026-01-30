bf99407a148a5162332fb30b394945196ecff537
AlekseiPavlov3 <aleksei_pavlov3@epam.com>
2026-01-30 11:24:43 +0300

    init

---
+++ Sorter/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Task: Sort a large text file content
+
+// Input: 100gb file. File text format is <Number>. <String>
+
+Console.WriteLine("Hello, World!");
